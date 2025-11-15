0204b9551d86dad9ee9525069f1c555500655403
Aneh Master <masteraneh247@gmail.com>
2025-11-15 07:28:58 +0000

    Application error: a client-side exception has occurred (see the browser

--- src/components/payment-method-icons.tsx
+++ src/components/payment-method-icons.tsx
@@ -4,15 +4,15 @@
 import React from 'react';
 import Image from 'next/image';
 
-const MtnLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/commons/9/93/MTN_Logo.svg" alt="MTN Mobile Money" width={48} height={48} className="object-contain" />;
-const VodafoneLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Vodafone_2017_logo.svg" alt="Vodafone Cash" width={48} height={48} className="object-contain" />;
-const AirtelTigoLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/commons/6/69/Airtel_Tigo_Logo.svg" alt="AirtelTigo Money" width={80} height={48} className="object-contain" />;
+const MtnLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/commons/9/93/MTN_Logo.svg" alt="MTN Mobile Money" fill style={{ objectFit: 'contain' }} />;
+const VodafoneLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/commons/0/0e/Vodafone_2017_logo.svg" alt="Vodafone Cash" fill style={{ objectFit: 'contain' }} />;
+const AirtelTigoLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/commons/6/69/Airtel_Tigo_Logo.svg" alt="AirtelTigo Money" fill style={{ objectFit: 'contain' }} />;
 
-const VisaLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg" alt="Visa" width={60} height={40} className="object-contain" />;
-const MastercardLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Mastercard_2019_logo.svg" alt="Mastercard" width={50} height={40} className="object-contain" />;
+const VisaLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/commons/5/5e/Visa_Inc._logo.svg" alt="Visa" fill style={{ objectFit: 'contain' }} />;
+const MastercardLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/commons/a/a4/Mastercard_2019_logo.svg" alt="Mastercard" fill style={{ objectFit: 'contain' }} />;
 
-const GcbLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/en/c/cb/GCB_Bank_logo.png" alt="GCB Bank" width={48} height={48} className="object-contain" />;
-const EcobankLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/commons/1/15/Ecobank_logo.svg" alt="Ecobank" width={80} height={48} className="object-contain" />;
+const GcbLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/en/c/cb/GCB_Bank_logo.png" alt="GCB Bank" fill style={{ objectFit: 'contain' }} />;
+const EcobankLogo = () => <Image src="https://upload.wikimedia.org/wikipedia/commons/1/15/Ecobank_logo.svg" alt="Ecobank" fill style={{ objectFit: 'contain' }} />;
 
 
 export const paymentMethodIcons: { [key: string]: React.FC } = {
